ab073c25178e2aaed875523d6a198b7554f7569e
Diessica <diessicarrc@gmail.com>
2023-04-19 21:40:13 -0300

    introdução a função

---
+++ index.js
@@ -0,0 +1,15 @@
+function imprimirOlaMundo() {
+    alert('Olá mundo')
+}
+
+imprimirOlaMundo();
+
+function imprimirNome (){
+let nome = prompt("Qual o seu nome?")
+alert(`Olá ${nome}`)
+}
+
+imprimirNome();
+imprimirNome('Rafael');
+imprimirNome('Pietro');
+imprimirNome('Cátia');
